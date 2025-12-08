8dedd1c61f27e69a36fc3b3165c208f62a4ea6f2
RPalaciosDev <rpalacios@csumb.edu>
2025-12-08 03:22:13 -0800

    removing build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rpala/OneDrive/Desktop/Fall2025/CST426/chess-base
-# Build directory: C:/Users/rpala/OneDrive/Desktop/Fall2025/CST426/chess-base/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
